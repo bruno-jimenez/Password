382f266db151e4ae3a937df3138be5ac25cbef5f
unknown <bruno-jimenez@laplateforme.io>
2023-01-31 16:13:38 +0100

    adding test for pwd verification

--- test verification.py
+++ test verification.py
@@ -1,9 +1,7 @@
-from json import *
-
-# create the list for all input for pwd generator
+# create the list for all input for pwd 
 
 alphabet_min=[chr(i) for i in range(ord('a'),ord('z')+1)]
-alphabet_max=[chr(i) for i in range(ord('A'),ord('Z')+1)]
+alphabet_maj=[chr(i) for i in range(ord('A'),ord('Z')+1)]
 chiffres = [ chr(i) for i in range(48,58) ]
 caracteres_speciaux = [ '%' , '_' , '-' , '!' , '$' , '^' , '&' , '#' , '(' , ')' , '[' , ']' , '=' , '@']
 
@@ -11,11 +9,12 @@ spc=input("do you want special caractere in your pwd ? Y/N ")
 
 if "Y" in spc:
     print(alphabet_min)
-    print(alphabet_max)
+    print(alphabet_maj)
     print(chiffres)
     print(caracteres_speciaux)
 
 if "N" in spc:
     print(alphabet_min)
-    print(alphabet_max)
+    print(alphabet_maj)
     print(chiffres)
+    
