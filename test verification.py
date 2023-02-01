22b48b344b417129c0b9a37ec0bd2eee7f8fce20
unknown <bruno-jimenez@laplateforme.io>
2023-02-01 16:10:23 +0100

    adding file who work at 75% just need to found how return to the beginning of the program when invalid password is written

--- test verification.py
+++ test verification.py
@@ -1,20 +1,73 @@
+#[-------------------------------------------]
 # create the list for all input for pwd 
+#[-------------------------------------------]
+alphabet_min="abcdefghijklmpnopqrstuvwxyz"
+alphabet_maj="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+chiffres = "1234567890"
+caracteres_speciaux = "!@#$%^&*()_-+=[]"
 
-alphabet_min=[chr(i) for i in range(ord('a'),ord('z')+1)]
-alphabet_maj=[chr(i) for i in range(ord('A'),ord('Z')+1)]
-chiffres = [ chr(i) for i in range(48,58) ]
-caracteres_speciaux = [ '%' , '_' , '-' , '!' , '$' , '^' , '&' , '#' , '(' , ')' , '[' , ']' , '=' , '@']
+#[-------------------------------------------]
+# cretion of all entry for the pwd 
+#[-------------------------------------------]
 
-spc=input("do you want special caractere in your pwd ? Y/N ")
+alphabet_min_entry= False
+alphabet_maj_entry= False
+caracteres_speciaux_entry= False
+chiffres_entry= False
+psw_entry= False
+verification_loop= True
 
-if "Y" in spc:
-    print(alphabet_min)
-    print(alphabet_maj)
-    print(chiffres)
-    print(caracteres_speciaux)
+#[-------------------------------------------]
+# function for verification pwd 
+#[-------------------------------------------]
+def verification():
 
-if "N" in spc:
-    print(alphabet_min)
-    print(alphabet_maj)
-    print(chiffres)
-    
+#[-------------------------------------------]
+# verification of all entry in the pwd 
+#[-------------------------------------------]
+    psw_input=list(input("Tape your password , the password must contain 8 caractere minimum , 1 number and 1 upper letter : "))
+    psw_input= str(psw_input)
+    psw= 0
+    psw = len(psw_input)
+
+    if psw >= 7:
+        print("your password contain 8 caractere minimum")
+        psw_entry= True
+    else:
+        psw_entry= False
+       
+    if chiffres in psw_input: 
+        print("your password contain number")
+        chiffres_entry= True 
+    else:
+        chiffres_entry= False   
+
+    if alphabet_maj in psw_input:
+        print("your password contain upper letter")
+        alphabet_maj_entry= True
+    else:
+        alphabet_maj_entry= False
+
+    if alphabet_min in psw_input: 
+        print("your password contain lower letter")
+        alphabet_min_entry= True
+    else:
+        alphabet_min_entry= False
+
+    if caracteres_speciaux in psw_input: 
+        print("your password contain special caratere")
+        caracteres_speciaux_entry= True
+    else:
+        caracteres_speciaux_entry= False
+        
+#[-------------------------------------------]
+# creation of the encrypting and validation of password or not
+#[-------------------------------------------]
+
+    while alphabet_min_entry == True and alphabet_maj_entry == True and psw_entry == True and caracteres_speciaux_entry == True and chiffres_entry == True:
+        print("your password is valid encrypting in progress")
+    else: 
+        if alphabet_min_entry == False or alphabet_maj_entry == False or psw_entry == False or caracteres_speciaux_entry == False or chiffres_entry == False:
+            print("invalid password write another password . :)")
+
+verification()
